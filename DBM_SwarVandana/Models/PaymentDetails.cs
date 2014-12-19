2c401b6752310341c0220651628fd037a6b68d72
Mohit Dagar <mohit.dagar@valuedgeindia.com>
2014-12-19 10:44:39 +0530

    Payment Details.

---
+++ DBM_SwarVandana/Models/PaymentDetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class PaymentDetails
+    {
+        public virtual long PaymentId { get; set; }
+        public virtual long EnrollmentId { get; set; }
+        public virtual long StudentId { get; set; }
+        public virtual string BankName { get; set; }
+        public virtual int PaymentMode { get; set; }
+        public virtual string PaymentDetails { get; set; }
+        public virtual DateTime? DateOfPayment { get; set; }
+        public virtual decimal AmountPaid { get; set; }
+        public virtual DateTime? DueDate { get; set; }
+        public virtual long AddBy { get; set; }
+        public virtual DateTime? AddDate { get; set; }
+        public virtual long ModifyBy { get; set; }
+        public virtual DateTime? ModifyDate { get; set; }
+
+        public PaymentDetails()
+        {
+            this.PaymentId = 0;
+            this.EnrollmentId = 0;
+            this.StudentId = 0;
+            this.BankName = string.Empty;
+            this.PaymentMode = 0;
+            this.PaymentDetails = string.Empty;
+            this.DateOfPayment = null;
+            this.AmountPaid = 0;
+            this.DueDate = null;
+            this.AddBy = 0;
+            this.AddDate = null;
+            this.ModifyBy = 0;
+            this.ModifyDate = null;
+
+        }
+    }
+}
---
+++ DBM_SwarVandana/ViewModel/PaymentDetailsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewModel
+{
+    public class PaymentDetailsViewModel : PaymentDetails
+    {
+        public PaymentDetailsViewModel()
+        {
+        }
+
+        public PaymentDetailsViewModel(PaymentDetails p)
+        {
+            this.PaymentId = p.PaymentId;
+            this.EnrollmentId = p.EnrollmentId;
+            this.StudentId = p.StudentId;
+            this.BankName = p.BankName;
+            this.PaymentMode = p.PaymentMode;
+            this.PaymentDetails = p.PaymentDetails;
+            this.DateOfPayment = p.DateOfPayment;
+            this.AmountPaid = p.AmountPaid;
+            this.DueDate = p.DueDate;
+            this.AddBy = p.AddBy;
+            this.AddDate = p.AddDate;
+            this.ModifyBy = p.ModifyBy;
+            this.ModifyDate = p.ModifyDate;
+        }
+    }
+}
