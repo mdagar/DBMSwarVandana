7497c50b30202436dd916bf761b88c99b962e996
Mohit Dagar <mohit.dagar@valuedgeindia.com>
2015-01-05 14:17:21 +0530

    BatchManagement Model and viewModel

---
+++ DBM_SwarVandana/ViewModel/BatchesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewModel
+{
+    public class BatchesViewModel : Batches
+    {
+        public BatchesViewModel()
+        { }
+
+        public BatchesViewModel(Batches b)
+        {
+            this.BatchId = b.BatchId;
+            this.Day = b.Day;
+            this.Timming = b.Timming;
+            this.StudentLinit = b.StudentLinit;
+            this.CreatedBy = b.CreatedBy;
+            this.CreatedDate = b.CreatedDate;
+            this.ModifyBy = b.ModifyBy;
+            this.ModifyDate = b.ModifyDate;
+        }
+    }
+}
---
+++ DBM_SwarVandana/ViewModel/StudentBatchMappingViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewModel
+{
+    public class StudentBatchMappingViewModel : StudentBatchMapping
+    {
+        public StudentBatchMappingViewModel()
+        { }
+        public StudentBatchMappingViewModel(StudentBatchMapping s)
+        {
+            this.ID = s.ID;
+            this.StudentId = s.StudentId;
+            this.EnrollmentId = s.EnrollmentId;
+            this.BatchId = s.BatchId;
+            this.CreatedBy = s.CreatedBy;
+            this.CreatedDate = s.CreatedDate;
+            this.ModifyBy = s.ModifyBy;
+            this.ModifyDate = s.ModifyDate;
+        }
+
+    }
+}
